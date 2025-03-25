fd4de48b155b655f892363906c398f36cd91f73a
Austino54 <austino8208@gmail.com>
2025-03-25 13:34:25 -0500

    Added more info to ReadMe

--- README.txt
+++ README.txt
@@ -3,3 +3,6 @@ Python 3
 Stable Baselines 3.0 - pip install stable-baselines3[extra]
 PyGame - pip install pygame
 Tensorboard - pip install tensorboard
+
+To use tensorboard:
+tensorboard --logdir=logs
--- goalLearn.py
+++ goalLearn.py
@@ -1,13 +1,14 @@
 import gymnasium as gym
 from stable_baselines3 import A2C
+from stable_baselines3 import PPO
 import os
 import time
 from stationaryGoalEnv import StationaryGoalEnv
 from randomGoalEnv import RandomGoalEnv
 
 # run 'tensorboard --logdir=logs' to see training logs. Replace 'logs' with whatever filepath the logs folder has
-models_dir = f"models/A2C_{int(time.localtime().tm_mon)}-{int(time.localtime().tm_mday)}-{int(time.localtime().tm_year)}_{int(time.localtime().tm_hour-6)}.{int(time.localtime().tm_min)}.{int(time.localtime().tm_sec)}"
-log_dir = f"logs/A2C_{int(time.localtime().tm_mon)}-{int(time.localtime().tm_mday)}-{int(time.localtime().tm_year)}_{int(time.localtime().tm_hour-6)}.{int(time.localtime().tm_min)}.{int(time.localtime().tm_sec)}"
+models_dir = f"models/Rand_{int(time.localtime().tm_mon)}-{int(time.localtime().tm_mday)}-{int(time.localtime().tm_year)}_{int(time.localtime().tm_hour-6)}.{int(time.localtime().tm_min)}.{int(time.localtime().tm_sec)}"
+log_dir = f"logs/Rand_{int(time.localtime().tm_mon)}-{int(time.localtime().tm_mday)}-{int(time.localtime().tm_year)}_{int(time.localtime().tm_hour-6)}.{int(time.localtime().tm_min)}.{int(time.localtime().tm_sec)}"
 
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
@@ -18,7 +19,7 @@ if not os.path.exists(log_dir):
 env = RandomGoalEnv(10, 10)
 env.reset()
 
-model = A2C("MlpPolicy", env, verbose=1, tensorboard_log=log_dir)
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_dir)
 
 # for some reason model.load doesn't load the model data. Must use model.set_parameters instead to get the parameters
 # model.load("models/PPO_2-18-2025_22.29.0/90000.zip")
@@ -27,7 +28,7 @@ model = A2C("MlpPolicy", env, verbose=1, tensorboard_log=log_dir)
 TIMESTEPS = 10000
 
 for i in range(1,10):
-    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="A2C")
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="Rand")
     model.save(f"{models_dir}/{TIMESTEPS*i}")
 
 env.close()
